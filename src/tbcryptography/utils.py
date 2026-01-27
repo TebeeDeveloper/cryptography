498f23cf8e8ad5580d4680e308bb577e0ea3ada0
TebeeDeveloper <tebeebmgo@gmail.com>
2026-01-27 11:58:00 +0700

    update auto use

--- src/tbcryptography/utils.py
+++ src/tbcryptography/utils.py
@@ -1,5 +1,6 @@
 from tbcryptography import atbash, caesar, EnigmaMachine, vigenere, tbc, tfsc
 from base64 import b85encode, b85decode
+import os
 
 def atbash_mc() -> None:
     while True:
@@ -22,6 +23,8 @@ def enigma_mc() -> None:
         if seed == "quit()":
             return None
         seed = eval(seed)
+        if not isinstance(seed, int):
+            continue
         enigma = EnigmaMachine(seed)
         mode = input("Nhập chế độ (encrypt/decrypt): ")
         msg = input("Nhập tin nhắn của bạn:\n>>>>")
@@ -55,14 +58,27 @@ def tbc_mc() -> None:
 def tfsc_mc() -> None:
     while True:
         mode = input("Nhập chế độ (encrypt/decrypt): ")
-        msg = input("Nhập tin nhắn của bạn:\n>>>>")
-        if msg == "quit()":
+        if mode == "quit()":
             return None
-        key = eval(input("Nhập khóa của bạn: "))
+        key = input("Nhập khóa (random/mykey): ")
+        if key == "quit()":
+            return None
+        if key == "mykey":
+            key = input("Nhập khóa:\n>>>> ")
+        if key == "random":
+            key = os.urandom(128)
+        msg = bytearray(input("Nhập tin nhắn của bạn:\n>>>> ").encode())
         if mode == "encrypt":
-            print(tfsc.process(msg, key).hex())
+            tfsc.encrypt(msg, key)
+            print(key)
+            print(msg.hex())
+            continue
+        if mode == "decrypt":
+            pt = tfsc.decrypt(msg, key)
+            print(pt)
+            continue
         else:
-            print(tfsc.process(msg, key).decode())
+            return None
 while True:
     machine = input("Nhập hệ thống bạn muốn dùng: ")
     if machine == "atbash":
